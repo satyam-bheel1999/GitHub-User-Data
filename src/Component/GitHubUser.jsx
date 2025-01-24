367c250723ecca1e3ef48fc1da0144c8c63a8883
Satyam Bheel <136482276+satyam-bheel1999@users.noreply.github.com>
2025-01-24 15:53:25 +0530

    styling to project

--- src/Component/GitHubUser.jsx
+++ src/Component/GitHubUser.jsx
@@ -30,12 +30,12 @@ function GitHubUser() {
 
   return (
     <div className='flex flex-col items-center w-screen h-screen bg-cover bg-center bg-no-repeat bg-[url("https://images.pexels.com/photos/2457284/pexels-photo-2457284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")]'>
-      <h1>GitHub User Finder</h1>
+      <h1 className="relative top-12 text-3xl text-white font-bold font-serif border-b-4 border-black">GitHub User Finder</h1>
 
       {/* display input field and user data on webpage */}
 
-      <div className="w-screen h-min flex flex-row justify-around border border-red-600">
-      <div className="border border-blue-700 min-w-96 min-h-auto">
+      <div className="w-screen h-min flex flex-row justify-around relative top-36">
+      <div className="border border-blue-700 w-lg h-96  bg-fuchsia-100 rounded-3xl text-center p-10">
           {error && <p style={{ color: "red" }}>{error}</p>}
 
           {user && (
@@ -51,15 +51,15 @@ function GitHubUser() {
             </div>
           )}
         </div>
-        <div className="border border-yellow-400 p-4">
+        <div className="border border-yellow-400 w-lg h-72 flex items-center justify-around bg-fuchsia-100 rounded-3xl">
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)} //set current user input value in username variable
             placeholder="Enter GitHub username"
-            className="p-2.5 bg-gray-300 rounded-2xl w-3xs"
+            className="p-2.5 bg-black text-white rounded-2xl w-56 "
           />
-          <button onClick={handleSearch} className="border border-yellow-300 p-2.5 rounded-2xl relative left-5.5">Search</button>
+          <button onClick={handleSearch} className="border border-violet-500 p-2.5 rounded-2xl w-36 text-black cursor-pointer">Search</button>
         </div>
 
         
